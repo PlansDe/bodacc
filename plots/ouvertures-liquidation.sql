e494cace6d586f2f811f915ab7e93928a7ae70d8
Anonymous Candidate <anon@example.com>
2021-10-18 14:05:23 +0000

    better query

--- plots/ouvertures-liquidation.sql
+++ plots/ouvertures-liquidation.sql
@@ -1,12 +1,13 @@
 -- SQLite
 SELECT DATE,CODEPOSTAL,VILLE,RCS,FORMEJURIDIQUE 
       FROM annonces 
-      WHERE NATURE = "Jugement d'ouverture de liquidation judiciaire"
-         AND FORMEJURIDIQUE != "S.C.I"
-         AND FORMEJURIDIQUE != "Société civile immobilière"
-         AND FORMEJURIDIQUE != "Société Civile Immobilière"
-         AND FORMEJURIDIQUE != "SOCIETE CIVILE IMMOBILIERE"
-         AND FORMEJURIDIQUE != "Société civile immobiliére"
+      WHERE NATURE = "jugement d'ouverture de liquidation judiciaire"
+         AND FORMEJURIDIQUE != "s.c.i"
+         AND FORMEJURIDIQUE != "s.c.i."
+         AND FORMEJURIDIQUE != "société civile immobilière"
+         AND FORMEJURIDIQUE != "société civile immobiliére"
+         AND FORMEJURIDIQUE != "societe civile immobiliere"
+         AND FORMEJURIDIQUE != "sci"
          AND DATE IS NOT NULL
          AND DATE != ""
          AND DATE >= "2008-01-01"
